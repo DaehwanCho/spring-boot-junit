cc333b8e06813f6bc8b3d586ffb98919f6fc1935
head1ton <head1ton@gmail.com>
2023-08-03 14:37:30 +0900

    1. 기본 세팅

---
+++ src/test/java/study/springbootjunit/SpringBootJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springbootjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
